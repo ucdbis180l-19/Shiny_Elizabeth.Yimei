1dc28b9ab38dbb2c5837d440f9272d4c4209d029
Elizabeth Roberts <etroberts@ucdavis.edu>
2019-05-10 00:00:35 +0000

    added comments

--- Shiny_Elizabeth.Yimei/ui.R
+++ Shiny_Elizabeth.Yimei/ui.R
@@ -13,10 +13,11 @@ shinyUI(fluidPage(
   sidebarLayout(#controls layout of sidebar
     
        sidebarPanel(radioButtons("trait", "Pick a trait to graph:", c("Amylose.content", "Protein.content" )), #gives ability to pick either amylose content or protein content to graph
-    radioButtons("category", "Pick a category to graph trait against", #gives option to 
-                 c("Region", "assignedPop")), selectInput("plot", "plot type", options)), 
+    radioButtons("category", "Pick a category to graph trait against", #gives option to pick Region or assignedPop
+                 c("Region", "assignedPop")), selectInput("plot", "plot type", options)), #gives option to select plot type
 
     
     mainPanel(conditionalPanel(condition = "input.plot == 'boxplot'", plotOutput("boxplot")), conditionalPanel(condition= "input.plot== 'violin'", plotOutput("violinplot"))))))
+#outputs plot depending on plot input, found existance of conditional panle from, https://stackoverflow.com/questions/50939906/conditional-main-panel-in-shiny 
   
 
